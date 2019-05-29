1f521f092d32fe48ebe8993d4ae69f0165277377
Viktor Recour <viktor.recour@student.vives.be>
2019-05-29 13:49:35 +0200

    Einde project

--- focusrecour.js
+++ focusrecour.js
@@ -1,50 +1,50 @@
-var youtubedl = require('youtube-dl');
 var express = require('express');
 var app = express();
+var http = require('http').createServer(app);
+var exec = require('child_process').exec;
+var io = require('socket.io')(http);
+var mqtt = require('mqtt')
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/assets'));
-
-
-// --- Routes ---
-// index
-app.get('/', function(req, res) {
-	console.log("Received GET 'index' request");
-	var dataIndex = {};
-	res.render('index', {data: dataIndex});
-});
+app.use(express.urlencoded());
+  
+var client  = mqtt.connect('mqtt://broker.hivemq.com');
 
 // directions
-app.get('/directions', function(req, res) {
+app.get('/', function(req, res) {
 	console.log("Received GET 'map' request");
 
-	var lat = 50.85433;
-	var lng = 2.86170;
+	var lat = 50.80585115;
+	var lng = 3.28435585537435;
 	var dataDirections = { lat: lat, lng: lng };
 	res.render('map', {data: dataDirections});
 });
 
-// youtube
-app.get('/youtube', function(req, res) {
-	console.log("Received GET 'youtube' request");
+// reboot
+app.get('/reboot', function(req, res) {
+	console.log("Received GET 'reboot' request");
+	restart();
 
-	// Download video
-	var video = youtubedl("https://www.youtube.com/watch?v=zp9Da20n0_k");
+	function execute(command, callback) {
+		exec(command, function(error, stdout, stderr) {
+			console.log("error: " + error);
+			callback(stdout);
+		});
+	}
 
-	video.on('info', function(info) {
-		console.log('Download started:');
-		console.log(`filename: ${info._filename}`);
-		console.log(`size: ${info.size}`);
-	});
+	function restart() {
+		console.log("Reboot");
+		execute('sudo reboot', function(callback) {});
+	}
+});
 
-	video.on('end', function() {
-		console.log('Download complete!');
-	});
-
-	var dataYoutube = {};
-	res.render('youtube', {data: dataYoutube});
+app.post('/', function(req, res) {
+	io.emit('updateCoordinates', req.body);
+	client.publish('focusrecour', JSON.stringify(req.body));
+	console.log(req.body);
 });
 
 // Create HTTP server
-app.listen(8080);
+http.listen(8080);
 console.log("Server listening on focusrecour:8080");
